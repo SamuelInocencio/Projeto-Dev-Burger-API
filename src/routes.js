931501be1450c8e04500981f67f2577e01931f39
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-02-13 20:02:00 -0300

    concluida aula 04 - Eslint+Prettier

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
-const { Router } = require('express')
-const { response } = require('./app')
+import {
+    Router
+} from 'express'
+
 
 const routes = new Router()
 
-routes.get('/',(request, response) => {
-    return response.status(200).json({message: 'Hello World!'})
+routes.get('/', (request, response) => {
+    return response.status(200).json({
+        message: 'Hello World!'
+    })
 })
 
-module.exports = routes
+export default routes
